a389d13ad7665f2ff64287afdce7005312755229
Martin Moradi <moradi.martin@gmail.com>
2024-07-15 22:46:41 +0200

    Add props to PatternCircleSVG

--- src/components/icons/pattern-circle.tsx
+++ src/components/icons/pattern-circle.tsx
@@ -4,6 +4,7 @@ const PatternCircleSVG = (props: React.SVGProps<SVGSVGElement>) => (
     height="944"
     viewBox="0 0 944 944"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <g stroke="#FFF" fill="none" fillRule="evenodd" opacity=".202">
       <circle cx="472" cy="472" r="235.5" />
