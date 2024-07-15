766653c68af1f3965a575d668b67e02a51f60456
Martin Moradi <moradi.martin@gmail.com>
2024-07-15 21:31:36 +0200

    Add SVG icons components

---
+++ src/components/icons/arrow-right.tsx
@@ -0,0 +1,13 @@
+const ArrowRightIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M1.322 1l5 5-5 5"
+      stroke="#D87D4A"
+      strokeWidth="2"
+      fill="none"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+
+export { ArrowRightIcon };
---
+++ src/components/icons/order-confirmation.tsx
@@ -0,0 +1,14 @@
+const OrderConfirmationIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <circle fill="#D87D4A" cx="32" cy="32" r="32" />
+      <path
+        stroke="#FFF"
+        strokeWidth="4"
+        d="m20.754 33.333 6.751 6.751 15.804-15.803"
+      />
+    </g>
+  </svg>
+);
+
+export { OrderConfirmationIcon };
