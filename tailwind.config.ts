45c7d4d913b3e50dd3cdb8a2af78763ab8e0d314
Martin Moradi <moradi.martin@gmail.com>
2024-07-15 17:36:42 +0200

    Theme config : colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -16,6 +16,15 @@ const config = {
       screens: {
         "2xl": "1400px",
       },
+    },
+    colors: {
+      primary: "hsl(22, 65%, 57%)",
+      dark: "hsl(0, 0%, 6%)",
+      light: "hsl(0, 0%, 95%)",
+      lighter: "hsl(0, 0%, 98%)",
+      secondary: "hsl(21, 94%, 75%)",
+      white: "hsl(0, 0%, 100%)",
+      black: "hsl(0, 0%, 0%)",
     },
     extend: {
       keyframes: {
@@ -35,6 +44,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
