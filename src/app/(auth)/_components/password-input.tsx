3fe494897347972c22dca3c06520c139cdbc680c
Martin Moradi <moradi.martin@gmail.com>
2024-07-28 13:03:03 +0200

    Add PasswordInput component

---
+++ src/app/(auth)/_components/password-input.tsx
@@ -0,0 +1,70 @@
+import type { Control, FieldPath, FieldValues } from "react-hook-form";
+import { Button } from "~/components/ui/button";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+
+interface PasswordInputProps<TFieldValues extends FieldValues> {
+  control: Control<TFieldValues>;
+  name: FieldPath<TFieldValues>;
+  label?: string;
+  placeholder?: string;
+  disabled?: boolean;
+}
+
+const PasswordInput = <TFieldValues extends FieldValues>({
+  control,
+  name,
+  label = "Password",
+  placeholder = "********",
+  disabled = false,
+}: PasswordInputProps<TFieldValues>) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <div className="relative">
+              <Input
+                {...field}
+                disabled={disabled}
+                placeholder={placeholder}
+                type={showPassword ? "text" : "password"}
+                className="pr-10"
+                aria-label="Password input field"
+              />
+              <button
+                type="button"
+                className="absolute right-10 top-0 h-full px-3 py-2 hover:bg-transparent"
+                onClick={() => setShowPassword(!showPassword)}
+                tabIndex={-1}
+                aria-label="Toggle password visibility"
+              >
+                {showPassword ? (
+                  <EyeOff className="h-6 w-6" />
+                ) : (
+                  <Eye className="h-6 w-6" />
+                )}
+              </button>
+            </div>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export { PasswordInput };
