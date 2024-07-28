14622ffda653e89f1452c6066256d301b97d067b
Martin Moradi <moradi.martin@gmail.com>
2024-07-28 17:23:32 +0200

    Refine Input component CSS

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "rounded-form flex h-[5.6rem] w-full border border-border-color bg-white px-10 py-2 text-base font-bold ring-offset-white [font-size:1.4rem] file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/40 focus-visible:border-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "rounded-form flex h-[5.6rem] w-full border border-border-color bg-white px-10 py-2 text-base font-bold ring-offset-white [font-size:1.4rem] file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-black/30 focus-visible:border-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
