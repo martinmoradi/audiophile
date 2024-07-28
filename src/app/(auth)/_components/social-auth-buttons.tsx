4ccb1af325b6e9b8185beefea7849c4af4238e3a
Martin Moradi <moradi.martin@gmail.com>
2024-07-28 13:02:50 +0200

    Add SocialAuthButtons component

---
+++ src/app/(auth)/_components/social-auth-buttons.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { GithubIcon } from "~/components/icons/socials/github";
+import { GoogleIcon } from "~/components/icons/socials/google";
+import { Button } from "~/components/ui/button";
+
+interface SocialAuthButtonsProps {
+  type: "signin" | "signup";
+}
+
+const SocialAuthButtons = ({ type }: Readonly<SocialAuthButtonsProps>) => {
+  const googleLabel =
+    type === "signin" ? "Sign in with Google" : "Sign up with Google";
+  const githubLabel =
+    type === "signin" ? "Sign in with Github" : "Sign up with Github";
+
+  return (
+    <div className="flex w-full items-center gap-x-2 hover:text-white">
+      <Button
+        className="w-full space-x-4"
+        variant="secondary"
+        onClick={() => null}
+        aria-label={googleLabel}
+      >
+        <GoogleIcon className="h-10 w-10" />
+        <span className="opacity-80">Google</span>
+        <span className="sr-only">{googleLabel}</span>
+      </Button>
+      <Button
+        className="w-full space-x-4"
+        variant="secondary"
+        onClick={() => null}
+        aria-label={githubLabel}
+      >
+        <GithubIcon className="h-10 w-10" />
+        <span className="opacity-80">Github</span>
+        <span className="sr-only">{githubLabel}</span>
+      </Button>
+    </div>
+  );
+};
+
+export { SocialAuthButtons };
